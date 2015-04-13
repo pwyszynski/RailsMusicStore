afa525e2ab3abbbe598ae0362b22b3da63bab1b2
pwyszynski <stuntmankg@gmail.com>
2015-04-13 15:07:33 +0200

    Adding/removing from cart works, but cant access cart when items are in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,34 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-require jquery
-require jquery_ujs
-require foundation
-require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require turbolinks
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$(window).load ->
+  $('a[data-target]').click (e) ->
+    e.preventDefault()
+    $this = $(this)
+    if $this.data('target') == 'Add to'
+      url = $this.data('addurl')
+      new_target = "Remove from"
+    else
+      url = $this.data('removeurl')
+      new_target = "Add to"
+    $.ajax url: url, type: 'put', success: (data) ->
+      $('.cart-count').html(data)
+      $this.find('span').html(new_target)
+      $this.data('target', new_target)
+
+$(window).load ->
+  $('#mycart .fi-x').click (e) ->
+    e.preventDefault()
+    $this = $(this).closest('a')
+    url = $this.data('targeturl')
+    $.ajax url: url, type: 'put', success: (data) ->
+      $('.cart-count').html(data)
+      $this.closest('.cart-movie').slideUp()
