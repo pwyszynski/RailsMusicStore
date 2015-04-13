efb56ff1e6bdea52871112226fa599c8d89ec412
pwyszynski <stuntmankg@gmail.com>
2015-04-13 16:25:17 +0200

    Cart properly displaying items in it

--- app/models/movie.rb
+++
@@ -1,8 +0,0 @@
-class Movie
-  include Mongoid::Document
-  field :title, type: String
-  field :release_year, type: Integer
-  field :price, type: Float
-  field :description, type: String
-  field :cover_url, type: String
-end
