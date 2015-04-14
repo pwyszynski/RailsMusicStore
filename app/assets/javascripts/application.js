e84439315b7e03495169280e26e9fc172d160603
pwyszynski <stuntmankg@gmail.com>
2015-04-14 19:28:11 +0200

    Precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,26 +18,26 @@
 
 $(function(){ $(document).foundation(); });
 
-$(window).load ->
-  $('a[data-target]').click (e) ->
-    e.preventDefault()
-    $this = $(this)
-    if $this.data('target') == 'Add to'
-      url = $this.data('addurl')
-      new_target = "Remove from"
-    else
-      url = $this.data('removeurl')
-      new_target = "Add to"
-    $.ajax url: url, type: 'put', success: (data) ->
-      $('.cart-count').html(data)
-      $this.find('span').html(new_target)
-      $this.data('target', new_target)
+// $(window).load ->
+//   $('a[data-target]').click (e) ->
+//     e.preventDefault()
+//     $this = $(this)
+//     if $this.data('target') == 'Add to'
+//       url = $this.data('addurl')
+//       new_target = "Remove from"
+//     else
+//       url = $this.data('removeurl')
+//       new_target = "Add to"
+//     $.ajax url: url, type: 'put', success: (data) ->
+//       $('.cart-count').html(data)
+//       $this.find('span').html(new_target)
+//       $this.data('target', new_target)
 
-$(window).load ->
-  $('#mycart .fi-x').click (e) ->
-    e.preventDefault()
-    $this = $(this).closest('a')
-    url = $this.data('targeturl')
-    $.ajax url: url, type: 'put', success: (data) ->
-      $('.cart-count').html(data)
-      $this.closest('.cart-movie').slideUp()
+// $(window).load ->
+//   $('#mycart .fi-x').click (e) ->
+//     e.preventDefault()
+//     $this = $(this).closest('a')
+//     url = $this.data('targeturl')
+//     $.ajax url: url, type: 'put', success: (data) ->
+//       $('.cart-count').html(data)
+//       $this.closest('.cart-movie').slideUp()
